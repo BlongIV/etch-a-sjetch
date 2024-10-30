36589678b594bd1e8ab469c39a9795838dda92f8
BlongIV <baker_long@outlook.com>
2024-10-29 21:41:35 -0700

    Added new grid button and fixed sizing issues

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
+const container = document.querySelector(".container");
+
 function initialGrid() {
     for (i = 0; i < 16; i++) {
-        const container = document.querySelector(".container");
         const newRow = document.createElement("div");
         newRow.className = "gridRow";
         for (x = 0; x < 16; x++) {
             const gridBox = document.createElement("div");
             gridBox.className = "gridBox";
+            let initialBoxPixels = 400 / 16;
+            gridBox.style.height = `${initialBoxPixels}px`;
+            gridBox.style.width = `${initialBoxPixels}px`;
             newRow.appendChild(gridBox);
         }
         container.appendChild(newRow);
     }
+    let gridBoxes = document.querySelectorAll(".gridBox");
+
+    gridBoxes.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "aqua";
+         });
+    });
 }
 
 
 initialGrid();
 
-const gridBoxes = document.querySelectorAll(".gridBox");
 
-gridBoxes.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "aqua";
-    });
-});
+const newGridButt = document.querySelector(".grid-butt");
 
+newGridButt.addEventListener("click", () => {
+    let newGridDimensionString;
+    do {
+        newGridDimensionString = prompt("Dimensions of new grid?");
 
+        newGridDimensionNum = Number(newGridDimensionString);
+    } while (isNaN(newGridDimensionNum) || newGridDimensionNum < 1 || newGridDimensionNum > 100);
+    createNewGrid(newGridDimensionNum);
+})
 
 function createNewGrid(dimension) {
-    const container = document.querySelector("container");
     container.replaceChildren();
     for (i = 0; i < dimension; i++) {
         const newRow = document.createElement("div");
@@ -34,16 +47,19 @@ function createNewGrid(dimension) {
         for (x = 0; x < dimension; x++) {
             const gridBox = document.createElement("div");
             gridBox.className = "gridBox";
+            let boxPixels = 400 / dimension;
+            gridBox.style.height = `${boxPixels}px`;
+            gridBox.style.width = `${boxPixels}px`;
             newRow.appendChild(gridBox);
         }
         container.appendChild(newRow);
     }
+    let gridBoxes = document.querySelectorAll(".gridBox");
+
+    gridBoxes.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "aqua";
+        });
+    });
 }
 
-const newGridButt = document.querySelector(".grid-butt");
-
-newGridButt.addEventListener("click", () => {
-    const newGridDimensionString = prompt("Dimensions of new grid?");
-    const newGridDimensionNum = Number(newGridDimensionString);
-    createNewGrid(newGridDimensionNum);
-})
